f3c3c5837bdf34eab725629de759ee425271c303
Cloaktastic <nguyendoangiatuan2003@gmail.com>
2026-06-09 16:14:56 +0700

    Finally done

--- tax_agent/graph.py
+++ tax_agent/graph.py
@@ -32,6 +32,7 @@ When answering, be precise about:
 
 Always note that your response is for educational purposes and the user
 should consult a licensed attorney for specific legal advice.
+Keep your answers short and concise.
 """
 
 
